da16015f9612017e80e7b45bbb2f73b52893bb36
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-26 20:54:28 +0600

    render and new template folder in Django

--- generator/views.py
+++ generator/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'generator/home.html', {'password': 'JubaerBhuiyan12'})
 
 def eggs(request):
     return HttpResponse('<h1>Eggs are awesome!</h1>')
