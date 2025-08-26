77a9ac5927f84a8b2212bb497eabae7b176f2a74
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-26 20:44:29 +0600

    Create user modified home page and egg page by using generator(views) and urls main file in Django

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('Hello World!')
+
+def eggs(request):
+    return HttpResponse('<h1>Eggs are awesome!</h1>')
