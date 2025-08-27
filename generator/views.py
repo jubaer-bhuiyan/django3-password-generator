006a9b3c3adfa1d7b7d65eeef07ee88218d6591a
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-27 21:51:38 +0600

    work with home.html and password.html

--- generator/views.py
+++ generator/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'generator/home.html', {'password': 'JubaerBhuiyan12'})
 
-def eggs(request):
-    return HttpResponse('<h1>Eggs are awesome!</h1>')
+def password(request):
+    return render(request, 'generator/password.html')
